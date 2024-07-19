9f3d49aa15233ceb34d9878d93f8ea9557303cda
qwerty-arun <arunkr.1804@gmail.com>
2024-07-19 23:12:17 +0530

    19/7/24

--- day26/prgm1/enum.c
+++ day26/prgm1/enum.c
@@ -2,7 +2,11 @@
 int main()
 {
 	enum days{mon,tues,wed}var;
-	// var = wed;
+	var = wed;
 	printf("%d\n",var);
+	enum point{x=8,y,z=5}p;
+	printf("%d\n",x);
+	printf("%d\n",y);
+	printf("%d\n",z);
 	return 0;
 }
